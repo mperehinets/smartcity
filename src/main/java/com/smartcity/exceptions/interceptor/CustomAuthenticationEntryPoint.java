08ad9b5f1eca38cd362c95d9cb8a876b25d81ee8
Andriy <37639528+sparksaway@users.noreply.github.com>
2019-06-05 22:04:45 +0300

    Fixed forbidden exception return json

---
+++ src/main/java/com/smartcity/exceptions/interceptor/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package com.smartcity.exceptions.interceptor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smartcity.exceptions.json.ExceptionResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException e) throws IOException, ServletException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(ExceptionResponse
+                .builder()
+                .url(request.getRequestURI())
+                .message("Access denied")
+                .build());
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(403);
+        response.getWriter().write(json);
+    }
+}
