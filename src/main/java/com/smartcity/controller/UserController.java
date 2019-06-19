00408462d02581deb8bd91cec838124619271772
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2019-06-19 16:56:28 +0300

    Set permission to update only for currently authenticated user

--- src/main/java/com/smartcity/controller/UserController.java
+++ src/main/java/com/smartcity/controller/UserController.java
@@ -2,14 +2,12 @@ package com.smartcity.controller;
 
 import com.smartcity.domain.Role;
 import com.smartcity.dto.UserDto;
-import com.smartcity.dto.transfer.ExistingRecord;
 import com.smartcity.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -46,14 +44,17 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/organization/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<UserDto> findUsersByOrganizationId(@PathVariable("id") Long organizationId){
+    public List<UserDto> findUsersByOrganizationId(@PathVariable("id") Long organizationId) {
         return userService.findByOrganizationId(organizationId);
     }
 
     @ResponseStatus(HttpStatus.OK)
-    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public UserDto updateUser(@Validated(ExistingRecord.class) @PathVariable("id") Long id, @RequestBody UserDto userDto) {
-        userDto.setId(id);
+    @PutMapping(value = "/update-profile",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto updateUser(Authentication authentication, @RequestBody UserDto userDto) {
+        Long userId = this.getCurrentUser(authentication).getId();
+        userDto.setId(userId);
         return userService.update(userDto);
     }
 
@@ -68,7 +69,7 @@ public class UserController {
     @PreAuthorize("hasAnyRole" +
             "(@securityConfiguration.getUserControllerActivateUserAllowedRoles())")
     @PostMapping(value = "/activate/{id}")
-    public boolean activateUser(@PathVariable("id") Long id){
+    public boolean activateUser(@PathVariable("id") Long id) {
         return userService.activate(id);
     }
 
@@ -77,14 +78,13 @@ public class UserController {
     public boolean updatePassword(@RequestBody String newPassword,
                                   Authentication authentication) {
 
-        Long userId = userService.findByEmail(authentication.getName()).getId();
-
+        Long userId = this.getCurrentUser(authentication).getId();
         return userService.updatePassword(userId, newPassword);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping(value = "/{id}/get-roles", produces = MediaType.APPLICATION_JSON_VALUE)
-    List<Role> getRolesByUserId(@PathVariable("id") Long id) {
+    public List<Role> getRolesByUserId(@PathVariable("id") Long id) {
         return userService.getRoles(id);
     }
 
@@ -92,12 +92,12 @@ public class UserController {
             "(@securityConfiguration.getUserControllerSetRolesByUserIdAllowedRoles())")
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(value = "/{id}/set-roles", consumes = MediaType.APPLICATION_JSON_VALUE)
-    boolean setRolesByUserId(@PathVariable("id") Long userId, @RequestBody List<Long> newRolesIds) {
+    public boolean setRolesByUserId(@PathVariable("id") Long userId, @RequestBody List<Long> newRolesIds) {
         return userService.setRoles(userId, newRolesIds);
     }
 
     @GetMapping(value = "/get-current", produces = MediaType.APPLICATION_JSON_VALUE)
-    UserDto getCurrentUser(Authentication authentication) {
+    public UserDto getCurrentUser(Authentication authentication) {
         return userService.findByEmail(authentication.getName());
     }
 
