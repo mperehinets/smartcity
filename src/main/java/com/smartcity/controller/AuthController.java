9bff99f162c3a17cd869abfa24da42e03728fc00
Oleksandr Musiuk <31615183+OleksandrMisiuk@users.noreply.github.com>
2019-06-27 16:45:58 +0300

    Added roles to model in signin method AuthController (#130)

--- src/main/java/com/smartcity/controller/AuthController.java
+++ src/main/java/com/smartcity/controller/AuthController.java
@@ -50,6 +50,7 @@ public class AuthController {
             Map<Object, Object> model = new HashMap<>();
             model.put("username", username);
             model.put("token", token);
+            model.put("roles", userDetailsService.getRoles(user.getId()));
             return ok(model);
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid username/password supplied");
